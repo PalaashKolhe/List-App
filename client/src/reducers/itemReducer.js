eea5a92ce8f66167ae90a6c82e87adb744f647a3
Palaash Kolhe <pkolhe@uwaterloo.ca>
2021-02-03 23:53:05 -0700

    react/redux working with  store. Added ItemModal

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -15,9 +15,20 @@ export default function(state = initialState, action) {
         case GET_ITEMS:
             return {
                 ...state
-            }
-            default:
-                return state;
+            };
+        case DELETE_ITEM:
+            return {
+                ...state,
+                items: state.items.filter(item => item.id !== action.payload)
+            };
+        case ADD_ITEM:
+            return {
+                ...state,
+                items: [action.payload, ...state.items]
+            };
+        default:
+        return state;
+        
     }
 
 }
